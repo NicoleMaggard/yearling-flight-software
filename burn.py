b6f210d3b099cd64043279f679008c95d807de97
NicoleMaggard <crmaggard@cpp.edu>
2022-07-15 14:20:08 -0700

    added a comment

--- burn.py
+++ burn.py
@@ -8,6 +8,7 @@ from adafruit_apds9960.apds9960 import APDS9960
 import adafruit_mcp9808
 import adafruit_tca9548a
 import mf_initialize'''
+#hi im writing a comment
 from pycubed import cubesat
 
 cubesat.burn(burn_num='2',dutycycle=0.10,duration=2)
